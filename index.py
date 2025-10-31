fff62b04788b25eb4b8a73c266188564302c8f31
thumod09 <thumod09@gmail.com>
2025-10-31 17:56:13 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,34 @@
-from wsgi import app
+from flask import Flask, request, jsonify
 
-# This code create by jobayar ahmed 
-# This code create by jobayar ahmed 
-# This code create by jobayar ahmed 
-# This code create by jobayar ahmed 
-# This code create by jobayar ahmed 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Like API is running ✅",
+        "status": "online"
+    })
+
+@app.route('/like')
+def like():
+    uid = request.args.get("uid")
+    if not uid:
+        return jsonify({"error": "Missing UID"})
+    
+    # ----- Giả lập dữ liệu API -----
+    player_name = "FB:ㅤ@GMRemyX"  # bạn có thể thay thành dữ liệu thật nếu có API riêng
+    likes_before = 1000
+    likes_after = 1020
+    given = likes_after - likes_before
+
+    return jsonify({
+        "UID": uid,
+        "Name": player_name,
+        "LikesBefore": likes_before,
+        "LikesAfter": likes_after,
+        "GivenByAPI": given,
+        "status": "success"
+    })
+
+if __name__ == '__main__':
+    app.run()
