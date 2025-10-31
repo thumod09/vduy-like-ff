2c1226417c84430488387eaacf497b19fe82cb89
thumod09 <thumod09@gmail.com>
2025-10-31 17:19:07 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,7 @@
+from wsgi import app
+
+# This code create by jobayar ahmed 
+# This code create by jobayar ahmed 
+# This code create by jobayar ahmed 
+# This code create by jobayar ahmed 
+# This code create by jobayar ahmed 
